01d11f34eab784a35945efa481af912858d7a4c4
schedowka <marina_molodkina@proton.me>
2024-10-15 19:16:06 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int sum = 0;
+        for (int i = 100; i<=500; i++ ) {
+            sum += i;
+        }
+        System.out.println("сумма всех чисел от 100 до 500: " +sum);
+    }
+}
